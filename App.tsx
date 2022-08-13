a033eecbbabbda82a92b9a083984a1e09d8f3b74
adrianeduard <adrian.eduard@gmail.com>
2022-08-13 12:31:37 +0300

    3 files modified

--- App.tsx
+++ App.tsx
@@ -1,11 +1,51 @@
 import * as React from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import './style.css';
 
+export interface ILoginForm {
+  username: string;
+  password: string;
+}
+
 export default function App() {
+  const formHook = useForm<ILoginForm>({
+    mode: 'onChange',
+  });
+  const onSubmit: SubmitHandler<ILoginForm> = (data) => console.log(data);
+
+  const handleOnKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      formHook.trigger();
+    }
+  };
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <form onSubmit={formHook.handleSubmit(onSubmit)}>
+      <div className="row">
+        <label>Username: </label>
+        <input
+          type="text"
+          {...formHook.register('username', { required: true })}
+          onKeyDown={handleOnKeyDown}
+        />
+        {formHook.formState.errors.username && (
+          <div className="err">Username is required</div>
+        )}
+      </div>
+      <div className="row">
+        <label>Password: </label>
+        <input
+          type="text"
+          {...formHook.register('password', { required: true })}
+          onKeyDown={handleOnKeyDown}
+        />
+        {formHook.formState.errors.password && (
+          <div className="err">Password is required</div>
+        )}
+      </div>
+      <div className="row">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 }
